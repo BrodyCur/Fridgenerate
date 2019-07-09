d0e3abdb55d1edf29a31e1cd6851fff8318636bf
Brody Currie <brodycurrie@gmail.com>
2019-07-09 14:52:31 -0400

    close to populating database with recipes

---
+++ fridgenerate/management/commands/consolidate_recipes.py
@@ -0,0 +1,40 @@
+import json
+import os
+import glob
+from fridgenerate.models import Recipe
+from django.core.management.base import BaseCommand, CommandError
+
+class Command(BaseCommand):
+
+  def handle(self, *args, **options):
+    
+    consolidated_recipes()
+    # for name in glob.glob('../../fixtures/Recipe_dataset/recipes/*.json'):
+    #   print(name)
+
+def consolidated_recipes():  
+  BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+  datasets = f"{BASE_DIR}/fixtures/Recipe_dataset/recipes/"
+
+  RECIPES = os.listdir(datasets)
+
+  new_recipes = {}
+
+  for x in RECIPES:
+    with open(f'{datasets}{x}') as r:
+      body = json.load(r)
+      
+
+      for key, val in body.items():
+        if val == {}:
+          pass
+        else:
+          # print(val["picture_link"])
+        
+          Recipe.objects.create(
+            title = val['title'],
+            image = val['picture_link'],
+            method = val['instructions'],
+            ingredients = val['ingredients']
+          )
