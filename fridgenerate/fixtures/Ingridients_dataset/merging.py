6cc03099b3532f9cc8e91dee2f7b5be2076959d5
Kevin <mail.kevinpatel@gmail.com>
2019-07-10 10:31:53 -0400

    created data.py to feed data into ingredient database

--- fridgenerate/fixtures/Ingridients_dataset/merging.py
+++ fridgenerate/fixtures/Ingridients_dataset/merging.py
@@ -8,7 +8,11 @@ def flatten(list_of_lists):
       flattened_list.append(y)
   return flattened_list
 
-def func():
+def convert_to_obj(ingredients, n):
+  for i in range(0, len(ingredients), n):
+    yield ingredients[i:i+n]
+
+def merge():
   allfiles_list = []
   merged_list = []
   for file in glob.glob("*.json"):
@@ -20,7 +24,9 @@ def func():
         print(file)  
   ingredient = flatten(r['ingredients'] for r in merged_list)
   unique_ingredients = list(dict.fromkeys(ingredient))
+  ingredient_json = [{'name': chunk[i] for i in range(len(chunk))}
+        for chunk in convert_to_obj(unique_ingredients, 1)]
   with open("merged.json", "w") as outfile:
-    json.dump(unique_ingredients, outfile)
+    json.dump(ingredient_json, outfile)
 
-func()
+merge()
