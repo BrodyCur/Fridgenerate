de9faedbe19f0b3652ccaa251fea4d67d67c8bb0
Kevin <mail.kevinpatel@gmail.com>
2019-07-12 10:43:24 -0400

    added fixtures folder

--- fridgenerate_django_app/api.py
+++ fridgenerate_django_app/api.py
@@ -6,20 +6,20 @@ import os
 import pdb
 
 def get_recipe(request, id):
-    response = requests.get(f"https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/{id}/information",
-                          headers={
-                            "X-RapidAPI-Host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
-                            "X-RapidAPI-Key": api_key
-                          })
-    recipe = json.loads(response.content)
-    recipe_title = recipe['title']
-    recipe_image = recipe['image']
-    recipe_instruction = recipe['instructions']
-    recipe_ingredients = recipe['extendedIngredients']
-    ingredients = []
-    for ingredient in recipe_ingredients: 
-      ingredients.append(ingredient['name'])
-    return JsonResponse({'name': recipe_title, 'image': recipe_image, 'ingredients': ingredients, 'instructions': recipe_instruction})
+  response = requests.get(f"https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/{id}/information",
+                        headers={
+                          "X-RapidAPI-Host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
+                          "X-RapidAPI-Key": api_key
+                        })
+  recipe = json.loads(response.content)
+  recipe_title = recipe['title']
+  recipe_image = recipe['image']
+  recipe_instruction = recipe['instructions']
+  recipe_ingredients = recipe['extendedIngredients']
+  ingredients = []
+  for ingredient in recipe_ingredients: 
+    ingredients.append(ingredient['name'])
+  return JsonResponse({'name': recipe_title, 'image': recipe_image, 'ingredients': ingredients, 'instructions': recipe_instruction})
 
 def rest_api(request):
   headers = {}
