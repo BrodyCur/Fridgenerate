68acb367e9206050c1552244eb32dba68616f859
Kevin <mail.kevinpatel@gmail.com>
2019-07-12 14:04:55 -0400

    added search autcomplete feature

--- fridgenerate_django_app/models.py
+++ fridgenerate_django_app/models.py
@@ -1,16 +1,16 @@
 from django.db import models
 
 class User(models.Model):
-  first_name = models.CharField(max_length=225)
-  last_name = models.CharField(max_length=225)
-  email = models.EmailField(max_length=70, unique=True)
+  first_name = models.CharField(max_length=50)
+  last_name = models.CharField(max_length=50)
+  email = models.EmailField(max_length=50, unique=True)
   created = models.DateTimeField
 
   def __str__(self):
     return (f'{self.first_name}, {self.last_name}')
 
 class Ingredient(models.Model):
-  name = models.CharField(max_length=225)
+  name = models.CharField(max_length=50)
   source = models.URLField(max_length=500, blank=True)
 
   def __str__(self):
@@ -25,10 +25,11 @@ class Fridge(models.Model):
     return self.owners
 
 class Recipe(models.Model):
-  title = models.CharField(max_length=225)
-  image = models.CharField(max_length=500, null=True)
+  title = models.CharField(max_length=50)
+  image = models.CharField(max_length=225, null=True)
   instructions = models.TextField(max_length=2000)
-  ingredients = models.ManyToManyField(Ingredient)
+  ingredients = models.CharField(max_length=50)
+  items = models.ManyToManyField(Ingredient)
 
   def __str__(self):
     return self.title
