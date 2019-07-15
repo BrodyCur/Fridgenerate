1b1ee36a3051fb474e39ba6943bbfcb73698cbd7
BrodyCur <brodycurrie@gmail.com>
2019-07-15 15:08:20 -0400

    wrote function for similar recipes get request

--- fridgenerate/urls.py
+++ fridgenerate/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('recipe_details/', api.get_recipe),
     path('recipes/', api.get_recipes_by_ingredients),
     path('random_recipes/', api.get_random_recipes),
+    path('similar_recipes/', api.get_similar_recipes),
     path('', include('fridgenerate_django_app.urls')),
     path('api-auth/', include('rest_framework.urls')),
     path('api/token/', TokenObtainPairView.as_view()),
--- fridgenerate_django_app/api.py
+++ fridgenerate_django_app/api.py
@@ -6,6 +6,64 @@ import os
 import pdb
 from .config import api_key
 
+
+def get_recipe(request):
+  recipe_id = json.loads(request.body)['data']['recipe_id']
+
+  # print('request', json.loads(request.body)['data']['recipe_id'])
+  # print("POST:", recipe_id)
+
+  response = requests.get(f"https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/{recipe_id}/information",
+                        headers={
+                          "X-RapidAPI-Host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
+                          "X-RapidAPI-Key": api_key
+                        })
+
+  recipe = json.loads(response.content)
+  recipe_title = recipe["title"]
+  recipe_image = recipe["image"]
+  recipe_instruction = recipe["instructions"]
+  recipe_ingredients = recipe["extendedIngredients"]
+
+  ingredients = []
+  for ingredient in recipe_ingredients: 
+    ingredients.append(ingredient["name"])
+
+  return JsonResponse({
+    'id': recipe_id,
+    'name': recipe_title,
+    'image': recipe_image,
+    'ingredients': ingredients,
+    'instructions': recipe_instruction
+  })
+
+
+def get_recipes_by_ingredients(request):
+  ingredients_query = json.loads(request.body.decode('utf-8'))
+  
+  # print("POST:", ingredients_query)
+
+  ingredients_url = f"https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients?number=15&ingredients={ingredients_query}"
+
+  response = requests.get(ingredients_url,
+    headers={
+      "X-RapidAPI-Host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
+      "X-RapidAPI-Key": api_key
+    }
+  )
+
+  recipe_list = json.loads(response.content)
+
+  return JsonResponse({
+    'recipes': [{
+      'id': r['id'],
+      'name': r['title'],
+      'image': r['image'],
+      'missing_ingredients': r['missedIngredientCount']
+    } for r in recipe_list]
+  })
+
+
 def get_random_recipes(request):
 
   random_url = f"https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/random?number={15}"
@@ -51,61 +109,29 @@ def get_random_recipes(request):
   # return JsonResponse(context)
 
 
-def get_recipe(request):
-  recipe_id = json.loads(request.body)['data']['recipe_id']
+def get_similar_recipes(request):
 
-  print('request', json.loads(request.body)['data']['recipe_id'])
-  print("POST:", recipe_id)
+  similar_url = f"https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/{626260}/similar"
 
-  response = requests.get(f"https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/{recipe_id}/information",
-                        headers={
-                          "X-RapidAPI-Host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
-                          "X-RapidAPI-Key": api_key
-                        })
-
-  recipe = json.loads(response.content)
-  recipe_title = recipe["title"]
-  recipe_image = recipe["image"]
-  recipe_instruction = recipe["instructions"]
-  recipe_ingredients = recipe["extendedIngredients"]
-
-  ingredients = []
-  for ingredient in recipe_ingredients: 
-    ingredients.append(ingredient["name"])
-
-  return JsonResponse({
-    'name': recipe_title,
-    'image': recipe_image,
-    'ingredients': ingredients,
-    'instructions': recipe_instruction
-  })
-
-
-def get_recipes_by_ingredients(request):
-  ingredients_query = json.loads(request.body.decode('utf-8'))
-  
-  print("POST:", ingredients_query)
-
-  ingredients_url = f"https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients?number=15&ingredients={ingredients_query}"
-
-  response = requests.get(ingredients_url,
+  response = requests.get(similar_url,
     headers={
       "X-RapidAPI-Host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
       "X-RapidAPI-Key": api_key
     }
   )
 
-  recipe_list = json.loads(response.content)
+  similar_recipes = json.loads(response.content)
 
+  # print(similar_recipes)
 
   return JsonResponse({
     'recipes': [{
       'id': r['id'],
       'name': r['title'],
       'image': r['image'],
-      'missing_ingredients': r['missedIngredientCount']
-    } for r in recipe_list]
+    } for r in similar_recipes]
   })
+
 
 
 def rest_api(request):
