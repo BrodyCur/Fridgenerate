72231b9369023b895d35788ad3b148d83e741d7b
BrodyCur <brodycurrie@gmail.com>
2019-08-14 15:57:58 -0400

    removed python app

--- fridgenerate_django_app/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Ingredient, Fridge, Recipe
-
-admin.site.register(Ingredient)
-admin.site.register(Fridge)
-admin.site.register(Recipe)
--- fridgenerate_django_app/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class FridgenerateAppConfig(AppConfig):
-    name = 'fridgenerate_django_app'
