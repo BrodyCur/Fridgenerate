7b4f37d162366386d06b6136aeba0b087996c6e9
BrodyCur <brodycurrie@gmail.com>
2019-09-27 16:00:39 -0400

    added cors permissions

--- fridgenerate/settings.py
+++ fridgenerate/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '1wfarrcga_65mom_cuwn^i!pmqiuwyycc*ky_@+on+l(1stown'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -56,10 +56,8 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'fridgenerate.urls'
 
-CORS_ORIGIN_WHITELIST = [
-    "http://localhost:8000",
-    "http://localhost:3000",
-]
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = False
 
 TEMPLATES = [
     {
