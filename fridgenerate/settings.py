ec666c266933272b1cde3a9aca387c50fd05fb20
BrodyCur <brodycurrie@gmail.com>
2019-09-27 16:20:57 -0400

    switch api_key access

--- fridgenerate/settings.py
+++ fridgenerate/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '1wfarrcga_65mom_cuwn^i!pmqiuwyycc*ky_@+on+l(1stown'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
